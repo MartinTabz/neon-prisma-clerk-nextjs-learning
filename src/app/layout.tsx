d7721b63837d35f40be4377165534edac5a5cada
Martin Šíl <mail@martinsil.cz>
2025-05-29 19:49:13 +0200

    Adds user authentication with Clerk
    Integrates Clerk for user authentication, providing a streamlined and secure way to manage user sign-up, sign-in, and user data.
    
    Also configures middleware to protect routes.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Open_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -24,12 +25,14 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="cs" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${openSans.variable} antialiased`}
-			>
-				<Providers>{children}</Providers>
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang="cs" suppressHydrationWarning>
+				<body
+					className={`${geistSans.variable} ${openSans.variable} antialiased`}
+				>
+					<Providers>{children}</Providers>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
