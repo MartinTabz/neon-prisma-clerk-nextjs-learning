a314429bb2509a29c1c6e0ca57e2d63bc35277a6
Martin Šíl <mail@martinsil.cz>
2025-05-29 19:22:37 +0200

    Adds provider wrapper component to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Open_Sans } from "next/font/google";
 import "./globals.css";
+import Providers from "@/components/providers";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -23,11 +24,11 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="cs">
+		<html lang="cs" suppressHydrationWarning>
 			<body
 				className={`${geistSans.variable} ${openSans.variable} antialiased`}
 			>
-				{children}
+				<Providers>{children}</Providers>
 			</body>
 		</html>
 	);
